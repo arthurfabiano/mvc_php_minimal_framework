aae35a4779df854140557c584fd35f75efc6c18d
Arthur Fabiano <arthur.masterdevelop@gmail.com>
2021-11-03 21:10:10 -0300

    Adicionando classes das models

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -5,5 +5,29 @@
 @section('content')
     <div class="container mt-5">
         <h1 class="text-center">Bem vindo {{ $nome }}</h1>
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Email</th>
+                <th scope="col">Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $u)
+                    <tr>
+                        <th scope="row">{{ $u->id }}</th>
+                        <td>{{ $u->name }}</td>
+                        <td>{{ $u->email }}</td>
+                        <td>
+                            <button type="button" class="btn btn-primary btn-sm">Editar</button>
+                            <button type="button" class="btn btn-danger btn-sm">Deletar</button>
+                        </td>
+                    </tr>
+                @endforeach
+              
+            </tbody>
+        </table>
     </div>
 @endsection
--- lib/cache/c9fc32ad874246420710844297097824327371ef.php
+++ lib/cache/c9fc32ad874246420710844297097824327371ef.php
@@ -3,6 +3,30 @@
 <?php $__env->startSection('content'); ?>
     <div class="container mt-5">
         <h1 class="text-center">Bem vindo <?php echo e($nome); ?></h1>
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Email</th>
+                <th scope="col">Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+                <?php $__currentLoopData = $users; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $u): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <tr>
+                        <th scope="row"><?php echo e($u->id); ?></th>
+                        <td><?php echo e($u->name); ?></td>
+                        <td><?php echo e($u->email); ?></td>
+                        <td>
+                            <button type="button" class="btn btn-primary btn-sm">Editar</button>
+                            <button type="button" class="btn btn-danger btn-sm">Deletar</button>
+                        </td>
+                    </tr>
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+              
+            </tbody>
+        </table>
     </div>
 <?php $__env->stopSection(); ?>
 <?php echo $__env->make('templates.default', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /opt/lampp/htdocs/mvc/app/views/home.blade.php ENDPATH**/ ?>
